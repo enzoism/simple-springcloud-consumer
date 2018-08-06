5091d599bf847f0d3acb6c211da57b7d4b29a999
unknown <enzoism>
2018-08-07 01:45:29 +0800

    first commit

---
+++ src/main/java/com/vincent/ServerConsume/ServerConsumeApplication.java
@@ -0,0 +1,12 @@
+package com.vincent.ServerConsume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerConsumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerConsumeApplication.class, args);
+	}
+}
